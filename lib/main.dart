69c5f199a3d0ee4ef5eab21084f7ddcb4a201b2e
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-04-15 10:20:05 +0400

    added empty toggleTheme method into appTheme and imported provider into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:releaf/shared/const/app_theme.dart';
 
 void main() {
--- lib/shared/const/app_theme.dart
+++ lib/shared/const/app_theme.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-class AppTheme {
+class AppTheme with ChangeNotifier {
   static Color primary = Colors.green;
   static late Color secondary;
   static late Color accent;
   static late TextStyle heading;
   static late TextStyle body;
+
+  void toggleTheme() {
+    notifyListeners();
+  }
 }
 
 class TestWidget extends StatelessWidget {
