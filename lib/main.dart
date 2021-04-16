706f749e26908bf5748f18c992ad13204660c7bf
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-04-16 22:36:25 +0400

    restructured main.dart and made theme color change smoothly.

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,43 @@ import 'package:releaf/shared/const/app_theme.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => AppTheme(
-              light: ThemeBase(
-                primary: Colors.green,
-                secondary: Colors.lightGreen[400],
-                accent: Colors.greenAccent,
-                backgroundColor: Colors.white,
-                heading: TextStyle(),
-                body: TextStyle(),
-              ),
-              dark: ThemeBase(
-                primary: Colors.lightGreen[400],
-                secondary: Colors.green,
-                accent: Colors.greenAccent,
-                backgroundColor: Colors.grey[850],
-                heading: TextStyle(),
-                body: TextStyle(),
-              ),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AppTheme(
+            light: ThemeBase(
+              primary: Colors.green,
+              secondary: Colors.lightGreen[400],
+              accent: Colors.greenAccent,
+              backgroundColor: Colors.white,
+              heading: TextStyle(),
+              body: TextStyle(),
             ),
-            lazy: false,
+            dark: ThemeBase(
+              primary: Colors.lightGreen[400],
+              secondary: Colors.green,
+              accent: Colors.greenAccent,
+              backgroundColor: Colors.grey[850],
+              heading: TextStyle(),
+              body: TextStyle(),
+            ),
           ),
-        ],
-        child: TestWidget(),
-      ),
+          lazy: false,
+        ),
+      ],
+      child: ThemedApp(),
     ),
   );
+}
+
+class ThemedApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = Provider.of<AppTheme>(context);
+
+    return MaterialApp(
+      home: TestWidget(),
+      theme: theme.getThemeData(),
+    );
+  }
 }
--- lib/shared/const/app_theme.dart
+++ lib/shared/const/app_theme.dart
@@ -33,35 +33,60 @@ class AppTheme with ChangeNotifier {
     required this.dark,
   }) {
     current = light;
+    _light = ThemeData(
+      primaryColor: light.primary,
+      backgroundColor: light.backgroundColor,
+      accentColor: light.accent,
+    );
+    _dark = ThemeData(
+      primaryColor: dark.primary,
+      backgroundColor: Colors.grey[850],
+      accentColor: dark.accent,
+    );
+    _currentThemeData = _light;
+  }
+
+  ThemeData _currentThemeData = ThemeData();
+  ThemeData _light = ThemeData();
+  ThemeData _dark = ThemeData();
+
+  getThemeData() => _currentThemeData;
+  setThemeData(bool light) {
+    _currentThemeData = light ? _light : _dark;
+    notifyListeners();
   }
 
   // TODO Make colour animate
   void toggleTheme() {
     if (current == light) {
       current = dark;
+      setThemeData(false);
     } else if (current == dark) {
       current = light;
+      setThemeData(true);
     }
     notifyListeners();
   }
 }
 
+// * TEST WIDGET - TEMP
 class TestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AppTheme theme = Provider.of<AppTheme>(context);
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        backgroundColor: theme.current.primary,
         title: Text('Test Widget'),
         centerTitle: true,
       ),
       body: Center(
         child: ThemedButton(
-          onPressed: () => print('tapped'), // TODO Test with empty function
+          onPressed: () => theme.toggleTheme(),
           text: Text('Click Me!'),
           color: theme.current.primary,
           shadowColor: Colors.black.withOpacity(0.6),
+          pressedShadowColor: Colors.greenAccent,
           shadowBlurRadius: 20,
         ),
       ),
@@ -76,8 +101,10 @@ class ThemedButton extends StatefulWidget {
   final Text text;
   final EdgeInsets? padding;
   final Color? color;
+  final Color? pressedColor;
   final double? borderRadius;
-  final Color shadowColor;
+  final Color? shadowColor;
+  final Color? pressedShadowColor;
   final double? shadowBlurRadius;
   final double? shadowSpreadRadius;
   final Offset? shadowOffset;
@@ -90,9 +117,11 @@ class ThemedButton extends StatefulWidget {
     this.padding,
     this.borderRadius,
     required this.shadowColor,
+    required this.pressedShadowColor,
     this.shadowBlurRadius,
     this.shadowSpreadRadius,
     this.shadowOffset,
+    this.pressedColor,
   });
 
   @override
@@ -120,7 +149,12 @@ class _ThemedButtonState extends State<ThemedButton> {
               BoxShadow(
                 blurRadius: widget.shadowBlurRadius ?? 10.0,
                 spreadRadius: widget.shadowSpreadRadius ?? 0,
-                color: widget.shadowColor,
+                color: Color.fromRGBO(
+                  widget.shadowColor!.red,
+                  widget.shadowColor!.green,
+                  widget.shadowColor!.blue,
+                  widget.shadowColor!.opacity,
+                ),
                 offset: widget.shadowOffset ?? Offset(0, 0),
               ),
             ],
