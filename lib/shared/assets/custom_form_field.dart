67bef5514767de68d634ddc95fd1c0b7b8de614e
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-04-25 12:36:49 +0400

    removed the custom input decoration

--- lib/shared/assets/custom_form_field.dart
+++ lib/shared/assets/custom_form_field.dart
@@ -1,39 +1,5 @@
 import 'package:flutter/material.dart';
 
-class CustomInputDecoration extends InputDecoration {
-  final String? label;
-  final String? hint;
-  final Widget? prefixIcon;
-  final Widget? suffixIcon;
-  final BuildContext context;
-  // final FocusNode focusNode;
-
-  CustomInputDecoration(
-    this.context, {
-    // required this.focusNode,
-    this.label,
-    this.hint,
-    this.prefixIcon,
-    this.suffixIcon,
-  }) : super(
-          contentPadding: EdgeInsets.fromLTRB(10, 15, 8, 20),
-          labelText: label,
-          // labelStyle: TextStyle(color: Colors.grey),
-          // color: focusNode.hasFocus
-          //     ? Theme.of(context).primaryColor
-          //     : Colors.grey),
-          hintText: hint,
-          enabledBorder: CustomWidgetBorder(color: Colors.grey, width: 1.2),
-          focusedBorder: CustomWidgetBorder(
-              color: Theme.of(context).primaryColor, width: 2.2),
-          errorBorder: CustomWidgetBorder(color: Colors.red[300], width: 1.5),
-          focusedErrorBorder:
-              CustomWidgetBorder(color: Colors.red[300], width: 2.4),
-          prefixIcon: prefixIcon,
-          suffix: suffixIcon,
-        );
-}
-
 class CustomWidgetBorder extends OutlineInputBorder {
   final Color? color;
   final double width;
