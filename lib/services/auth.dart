da480ade5f7ed9149d065d2f87f52d42235de717
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-04-25 13:51:21 +0400

    created auth service class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,15 +4,71 @@ import 'package:provider/provider.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? getUser() => _auth.currentUser;
 
   // auth change user stream
+  Stream<User?> get user {
+    return _auth.authStateChanges();
+  }
+
+  // Send verification email
+  Future sendVerificationEmail() async {
+    if (_auth.currentUser != null) {
+      await _auth.currentUser!.sendEmailVerification();
+    } else {
+      print('user is null');
+    }
+  }
 
   // register with email and password
+  Future register({required String email, required String password}) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      return e.toString();
+    }
+  }
 
   // log in with email and password
+  Future logIn({required String email, required String password}) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      return e.toString();
+    }
+  }
 
   // log in with Google
 
   // log out
+  Future logOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      return e.toString();
+    }
+  }
 
+  final Map _authErrors = {
+    '[firebase_auth/invalid-email] The email address is badly formatted.':
+        'Invalid Email',
+    '': '',
+    'Password should be at least 6 characters':
+        'Password should be at least 6 characters',
+    '[firebase_auth/email-already-in-use] The email address is already in use by another account.':
+        'Email address is already in use',
+    '': 'Unkown Error',
+    null: 'Unkown Error',
+  };
+  String getError([String? e]) {
+    var error = _authErrors[e];
+    print(e);
+    return error ?? e;
+  }
 }
