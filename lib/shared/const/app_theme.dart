fc0947262a4fac39c8538a02c1beb149c8e6a900
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-04-27 11:34:24 +0400

    added flat button

--- lib/shared/const/app_theme.dart
+++ lib/shared/const/app_theme.dart
@@ -194,3 +194,126 @@ class _ThemedButtonState extends State<ThemedButton>
     );
   }
 }
+
+// TODO Make option for enableFeedback (vibrations)
+class ThemedFlatButton extends StatefulWidget {
+  // * Variables
+  final String label;
+  final TextStyle? style;
+  final bool? notAllCaps;
+  final double? gap;
+  final Widget icon;
+  final VoidCallback? onPressed;
+  final EdgeInsets? padding;
+  final EdgeInsets? margin;
+  final Color? color;
+  final Color? pressedColor;
+  final double? borderRadius;
+
+  // * Constructors
+  final bool iconButton;
+  ThemedFlatButton({
+    required this.label,
+    this.style,
+    this.notAllCaps,
+    required this.onPressed,
+    this.padding,
+    this.margin,
+    this.color,
+    this.pressedColor,
+    this.borderRadius,
+  })  : icon = Container(height: 0, width: 0),
+        iconButton = false,
+        gap = 0.0;
+
+  ThemedFlatButton.icon({
+    required this.label,
+    this.style,
+    this.notAllCaps,
+    this.gap,
+    required this.icon,
+    required this.onPressed,
+    this.padding,
+    this.margin,
+    this.color,
+    this.borderRadius,
+    this.pressedColor,
+  }) : iconButton = true;
+
+  @override
+  _ThemedFlattButtonState createState() => _ThemedFlattButtonState();
+}
+
+class _ThemedFlattButtonState extends State<ThemedFlatButton>
+    with SingleTickerProviderStateMixin {
+  // Variables
+  bool _initialised = false;
+  late Color _color;
+  late Color _shadowColor;
+  late double _shadowBlurRadius;
+  late double _shadowSpreadRadius;
+  late Offset _shadowOffset;
+
+  // Animate When tapped down
+  void _animateDown() {
+    _color = widget.pressedColor ?? Theme.of(context).accentColor;
+  }
+
+  // Animate When Tapped up
+  void _animateUp() {
+    _color = widget.color ?? Theme.of(context).primaryColor;
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_initialised) {
+      _animateUp();
+      _initialised = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onPressed,
+      onTapDown: (TapDownDetails) => setState(() {
+        _animateDown();
+        SystemSound.play(SystemSoundType.click);
+      }),
+      onTapUp: (TapUpDetails) => setState(() => _animateUp()),
+      onTapCancel: () => setState(() => _animateUp()),
+      // * AnimatedContainer
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 133),
+        margin: widget.margin ?? EdgeInsets.all(0),
+        padding: widget.padding ??
+            EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 13.0,
+            ),
+        decoration: BoxDecoration(
+          color: _color,
+          borderRadius: BorderRadius.circular(widget.borderRadius ?? 10),
+        ),
+        // Text
+        // * Text
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            widget.icon,
+            SizedBox(width: (widget.iconButton) ? (widget.gap ?? 8.0) : 0.0),
+            Text(
+              (widget.notAllCaps ?? false)
+                  ? widget.label
+                  : widget.label.toUpperCase(),
+              style: widget.style ?? Theme.of(context).textTheme.button,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
