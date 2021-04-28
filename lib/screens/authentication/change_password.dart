3f287c1eebf1dbf85b906b8342e35faa6229a270
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-04-28 17:35:05 +0400

    added UI to change password

--- lib/screens/authentication/change_password.dart
+++ lib/screens/authentication/change_password.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:releaf/services/auth.dart';
 import 'package:releaf/shared/const/app_theme.dart';
 import 'package:releaf/shared/assets/custom_form_field.dart';
 
@@ -11,9 +12,37 @@ class ChangePassword extends StatefulWidget {
 }
 
 class _ChangePasswordState extends State<ChangePassword> {
+  final _auth = AuthService();
   final _formKey = new GlobalKey<FormState>();
-  FocusNode _emailFocusNode = new FocusNode();
-  String _email = '';
+  FocusNode _oldPasswordFocusNode = new FocusNode();
+  FocusNode _newPasswordFocusNode = new FocusNode();
+  FocusNode _confirmPasswordFocusNode = new FocusNode();
+  String _password = '';
+  String? _error;
+
+  @override
+  void initState() {
+    _oldPasswordFocusNode.addListener(() {
+      setState(() {});
+    });
+    _newPasswordFocusNode.addListener(() {
+      setState(() {});
+    });
+    _confirmPasswordFocusNode.addListener(() {
+      setState(() {});
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _formKey.currentState!.dispose();
+    _oldPasswordFocusNode.dispose();
+    _newPasswordFocusNode.dispose();
+    _confirmPasswordFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +89,159 @@ class _ChangePasswordState extends State<ChangePassword> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [],
+                        children: [
+                          // * Enter old paassword
+                          TextFormField(
+                            focusNode: _oldPasswordFocusNode,
+                            initialValue: _password,
+                            obscureText: false,
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return 'Please enter a password';
+                              } else if (val.length < 8) {
+                                return 'Password needs to be at least 8 characters';
+                              }
+                              // TODO check if it matches old password
+                            },
+                            autocorrect: false,
+                            decoration: _theme.inputDecoration.copyWith(
+                              labelText: 'Old Password',
+                              labelStyle: TextStyle(
+                                color: _oldPasswordFocusNode.hasFocus
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey,
+                              ),
+                              focusedBorder: CustomWidgetBorder(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2.2),
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: _oldPasswordFocusNode.hasFocus
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: _oldPasswordFocusNode.hasFocus
+                                      ? Theme.of(context).primaryColor
+                                      : Colors.grey,
+                                ),
+                                onPressed: () => print('clear'),
+                              ),
+                            ),
+                          ),
+                          // * Enter new password
+                          SizedBox(height: 20),
+                          TextFormField(
+                            focusNode: _newPasswordFocusNode,
+                            initialValue: _password,
+                            obscureText: false,
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return 'Please enter a password';
+                              } else if (val.length < 8) {
+                                return 'Password needs to be at least 8 characters';
+                              }
+                              // TODO Check it is not equal to the old password
+                            },
+                            autocorrect: false,
+                            onChanged: (val) {
+                              setState(() => _password = val);
+                            },
+                            decoration: _theme.inputDecoration.copyWith(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                color: _newPasswordFocusNode.hasFocus
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey,
+                              ),
+                              focusedBorder: CustomWidgetBorder(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2.2),
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: _newPasswordFocusNode.hasFocus
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: _newPasswordFocusNode.hasFocus
+                                      ? Theme.of(context).primaryColor
+                                      : Colors.grey,
+                                ),
+                                onPressed: () => print('clear'),
+                              ),
+                            ),
+                          ),
+                          // * Confirm New Password
+                          SizedBox(height: 20),
+                          TextFormField(
+                            focusNode: _confirmPasswordFocusNode,
+                            obscureText: false,
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return 'Please enter a password';
+                              } else if (val.length < 8) {
+                                return 'Password needs to be at least 8 characters';
+                              } else if (val != _password) {
+                                return 'Passwords do not match';
+                              }
+                            },
+                            autocorrect: false,
+                            onChanged: (val) {
+                              setState(() {});
+                            },
+                            decoration: _theme.inputDecoration.copyWith(
+                              labelText: 'Confirm Password',
+                              labelStyle: TextStyle(
+                                color: _confirmPasswordFocusNode.hasFocus
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey,
+                              ),
+                              focusedBorder: CustomWidgetBorder(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2.2),
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: _confirmPasswordFocusNode.hasFocus
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.grey,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: _confirmPasswordFocusNode.hasFocus
+                                      ? Theme.of(context).primaryColor
+                                      : Colors.grey,
+                                ),
+                                onPressed: () => print('clear'),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              height:
+                                  (_error == null || _error == '') ? 30 : 0),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical:
+                                  (_error == null || _error == '') ? 0 : 13,
+                            ),
+                            child: Text('$_error',
+                                style: TextStyle(
+                                  fontSize:
+                                      (_error == null || _error == '') ? 0 : 14,
+                                  color: Colors.red[800],
+                                ),
+                                textAlign: TextAlign.center),
+                          ),
+                          ThemedButton(
+                            label: 'Change Password',
+                            onPressed: () {},
+                          )
+                        ],
                       ),
                     ),
                   ],
