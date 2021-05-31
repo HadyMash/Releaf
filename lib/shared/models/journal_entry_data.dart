61a7e48ad6920657b56293c8b7c6a3fae81e0551
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-05-31 19:58:38 +0400

    added JournalEntryData class used in previous commits of database.dart

---
+++ lib/shared/models/journal_entry_data.dart
@@ -0,0 +1,11 @@
+class JournalEntryData {
+  final String date;
+  final String entryText;
+  final int feeling;
+
+  JournalEntryData({
+    required this.date,
+    required this.entryText,
+    required this.feeling,
+  });
+}
