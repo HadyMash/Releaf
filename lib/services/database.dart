5e658e74f18c3f6205141ce4fd22cfe6be358241
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-05-31 13:24:56 +0400

    added methods to add and get  journal entries.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,9 +1,51 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:releaf/shared/models/journal_entry_data.dart';
 
 class DatabaseService {
   final String uid;
-  DatabaseService({required this.uid});
+  late final FirebaseFirestore firestore;
+  DatabaseService({required this.uid}) {
+    firestore = FirebaseFirestore.instance;
+    journal = firestore.doc('journal/$uid');
+  }
 
-  final CollectionReference settingsCollection =
-      FirebaseFirestore.instance.collection('settings');
+  // * Journal
+  late DocumentReference<Object?> journal;
+
+  // TODO add new entry
+  Future addNewJournalEntry(String date, String entryText, int feeling) async {
+    try {
+      await journal.set({
+        "$date": {
+          "entryText": entryText,
+          "feeling": feeling,
+        },
+      }, SetOptions(merge: true));
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // TODO delete entry
+
+  // TODO edit an entry
+
+  // TODO get entries
+  Future<List<JournalEntryData>> getJournalEntries() async {
+    List<JournalEntryData> entries = [];
+
+    await journal.get().then((document) {
+      Map data = (document.data() as Map);
+      data.forEach((key, value) {
+        print('adding entry');
+        entries.add(JournalEntryData(
+          date: key,
+          entryText: value['entryText'],
+          feeling: value['feeling'],
+        ));
+      });
+    });
+
+    return entries;
+  }
 }
