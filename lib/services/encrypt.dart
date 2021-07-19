741e7fc2f2d1ec5060de12e691b77052ce9f08e3
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-07-19 16:41:33 +0400

    added image encryption but it doesn't work.

--- lib/services/encrypt.dart
+++ lib/services/encrypt.dart
@@ -1,23 +1,30 @@
+import 'dart:typed_data';
 import 'package:encrypt/encrypt.dart';
 
 class EncryptService {
   final String uid;
   final Key key;
-  EncryptService(this.uid) : key = Key.fromUtf8('${uid}abcd');
+  final Key incorrectImageKey;
+  EncryptService(this.uid)
+      : key = Key.fromUtf8('${uid}abcd'),
+        incorrectImageKey = Key.fromUtf8(
+            '${String.fromCharCodes(uid.runes.toList().reversed)}1234');
 
   // encrypt
-  String encrypt(String text) {
+  String encrypt(String text, {bool? incorrect}) {
     final iv = IV.fromLength(16);
-    final encrypter = Encrypter(AES(key));
+    final encrypter =
+        Encrypter(AES((incorrect ?? false) == true ? incorrectImageKey : key));
 
     final encrypted = encrypter.encrypt(text, iv: iv);
     return encrypted.base64;
   }
 
   // decrypt
-  String decrypt(String encryptedText) {
+  String decrypt(String encryptedText, {bool? incorrect}) {
     final iv = IV.fromLength(16);
-    final encrypter = Encrypter(AES(key));
+    final encrypter =
+        Encrypter(AES((incorrect ?? false) == true ? incorrectImageKey : key));
 
     final encrypted = Encrypted.from64(encryptedText);
     final decrypted = encrypter.decrypt(encrypted, iv: iv);
@@ -25,7 +32,39 @@ class EncryptService {
     return decrypted;
   }
 
-  // TODO encrypt image
+  // encrypt image
+  Uint8List encryptImage(Uint8List image) {
+    // Properly Encrypt the Image
+    List<String> encryptedImage = [];
+    for (int bit in image) {
+      encryptedImage.add(encrypt(bit.toString()));
+    }
 
-  // TODO decrypt image
+    // Incorrectly Decrypt the Image
+    List<int> incorrectDecryption = [];
+    for (String encryptedBit in encryptedImage) {
+      incorrectDecryption
+          .add(int.parse(decrypt(encryptedBit, incorrect: true)));
+    }
+
+    return Uint8List.fromList(incorrectDecryption);
+  }
+
+  // decrypt image
+  Uint8List decryptImage(Uint8List encryptedImage) {
+    List<String> incorrectDecryption = [];
+
+    // Incorrectly Ecrypt the Image
+    for (int bit in encryptedImage) {
+      incorrectDecryption.add(encrypt(bit.toString(), incorrect: true));
+    }
+
+    // Correctly Decrypt Image
+    List<int> decyrption = [];
+    for (String bit in incorrectDecryption) {
+      decyrption.add(int.parse(decrypt(bit)));
+    }
+
+    return Uint8List.fromList(decyrption);
+  }
 }
--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -1,6 +1,9 @@
+import 'dart:ffi';
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:releaf/services/encrypt.dart';
 
 class StorageService {
   final String uid;
@@ -14,10 +17,12 @@ class StorageService {
       {required List<Uint8List> pictures, required String entryID}) async {
     try {
       // TODO encrypt pictures
+      EncryptService encryptService = EncryptService(uid);
       final ref = storage.ref('$uid/$entryID');
-      pictures.forEach((picture) {
+      for (Uint8List picture in pictures) {
+        // Uint8List encryptedPicture = encryptService.encryptImage(picture);
         ref.child('${DateTime.now().toString()}.txt').putData(picture);
-      });
+      }
     } catch (e) {
       print(e.toString());
       return e;
@@ -43,20 +48,24 @@ class StorageService {
   // get picture(s)
   Future getPictures(String entryID) async {
     try {
+      EncryptService encryptService = EncryptService(uid);
       final ref = storage.ref(uid).child(entryID);
       List<Uint8List> pictures = [];
 
       await ref.listAll().then((result) async {
         for (var picReference in result.items) {
-          Uint8List? pic = await ref.child(picReference.name).getData();
+          Uint8List? pic = (await ref.child(picReference.name).getData());
           if (pic == null) {
             // TODO make no picture found picture
             var url = Uri.parse(
                 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png');
             var response = await http.get(url);
             pic = response.bodyBytes;
+            pictures.add(pic);
+          } else {
+            // var decryptedPic = encryptService.decryptImage(pic);
+            pictures.add(pic);
           }
-          pictures.add(pic);
         }
       });
       return pictures;
